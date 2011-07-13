5157d9de6318937d60d7c09ebec5606516516bb1
socjopata <socjopata@socjopata-selleo.(none)>
2011-07-13 16:08:13 +0200

    bare bone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharMaker::Application.config.secret_token = 'fe7b79713a08d1033ce3bfa42fb5d502551628aa3fc7c16fb8a5e759223b15ddcbaeaae6d1a00159e8d59eec4192a9713dcf664d4ea94ceac5fce1282943135f'
