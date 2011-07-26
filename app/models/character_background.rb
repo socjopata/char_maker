92734d9f397d96894dfb77b11f97ddc75bfd6fdd
socjopata <socjopata@socjopata-selleo.(none)>
2011-07-26 15:04:42 +0200

    traits and social classes

--- app/models/character_background.rb
+++ app/models/character_background.rb
@@ -14,17 +14,30 @@ class CharacterBackground < ActiveRecord::Base
         else
           nil
       end
-
     end
   end
 
 
   def attach_a_curse(num)
-    #TODO
+    traits << Trait.find_by_name(Trait::DICE_RESULT_FOR_CURSE[num])
   end
 
   def attach_a_blessing(num)
-    #TODO
+    traits << Trait.find_by_name(Trait::DICE_RESULT_FOR_BLESSING[num])
+  end
+
+  def draw_a_social_class
+    if self.new_record?
+      dice_roll = 1 + rand(Trait::DICE_TYPE)
+      case dice_roll
+        when Trait::CURSE_RANGE then
+          attach_a_curse(1 + rand(Trait::DICE_TYPE))
+        when Trait::BLESSING_RANGE then
+          attach_a_blessing(1 + rand(Trait::DICE_TYPE))
+        else
+          nil
+      end
+    end
   end
 
 
--- app/models/social_class.rb
+++ app/models/social_class.rb
@@ -2,4 +2,39 @@ class SocialClass < ActiveRecord::Base
  #TODO ask Artur if he wants description of a social class presented somewhere
 
   has_and_belongs_to_many :character_backgrounds
+
+  DICE_TYPE = 30 #k30
+  DICE_RESULT = {
+  1 => "Niewolnik",
+  2 => "Niewolnik",
+  3 => "Niewolnik",
+  4 => "Chłop Ubogi",
+  5 => "Chłop Ubogi",
+  6 => "Chłop Ubogi",
+  7 => "Chłop Bogaty",
+  8 => "Chłop Bogaty",
+  9 => "Chłop Bogaty",
+  10 => "Mieszczanin Ubogi",
+  11 => "Mieszczanin Ubogi",
+  12 => "Mieszczanin Ubogi",
+  13 => "Mieszczanin Ubogi",
+  14 => "Mieszczanin Ubogi",
+  15 => "Mieszczanin",
+  16 => "Mieszczanin",
+  17 => "Mieszczanin",
+  18 => "Mieszczanin",
+  19 => "Mieszczanin",
+  20 => "Mieszczanin Bogaty",
+  21 => "Mieszczanin Bogaty",
+  22 => "Mieszczanin Bogaty",
+  23 => "Szlachcic Zaściankowy",
+  24 => "Szlachcic Zaściankowy",
+  25 => "Szlachcic Zaściankowy",
+  26 => "Szlachcic",
+  27 => "Szlachcic",
+  28 => "Szlachcic",
+  29 => "Wielmoża",
+  30 => "Wielmoża"
+  }
+
 end
