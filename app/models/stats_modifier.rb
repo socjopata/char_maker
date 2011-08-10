5a0d0fb47fc77e7a77ee60c070e56c8400e9c587
socjopata <socjopata@socjopata-selleo.(none)>
2011-08-10 11:28:00 +0200

    todo for statsmodifier

--- app/models/stats_modifier.rb
+++ app/models/stats_modifier.rb
@@ -1,3 +1,10 @@
 # -*- encoding : utf-8 -*-
 class StatsModifier < Statistics
+
+
+  #TODO
+  #how about making it so StatsModifier that belongs_to :resource and on the other end of association, like Profession,
+  #we have like has_one :stats_modifier, :as => :resource ?
+  #think about it.
+
 end
