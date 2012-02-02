71dc17a0c817fe97711c0328f0870370c9b76bfa
socjopata <socjopata@socjopata-pc.(none)>
2012-02-02 21:22:40 +0100

    simple captcha

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 class MessagesController < ApplicationController
+  include SimpleCaptcha::ControllerHelpers
 
   def new
     @message = Message.new
@@ -8,11 +9,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params[:message])
-    if @message.valid?
+    if @message.valid? && (simple_captcha_valid? or current_user)
       AlantarMailer.contact_us(@message).deliver
       flash[:notice] = "Wiadomość wysłana! Dzięki za kontakt."
       redirect_to root_url
     else
+      flash[:alert] = "Coś poszło nie tak jak powinno. Napewno przepisałeś poprawnie kod z obrazka?"
       render :action => 'new'
     end
   end
