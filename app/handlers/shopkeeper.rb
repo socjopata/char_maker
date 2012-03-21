e908f18b3f15850f339439632606419b977b02fe
socjopata <socjopata@gmail.com>
2012-03-21 16:43:54 +0100

    introducing handlers

---
+++ app/handlers/shopkeeper.rb
@@ -0,0 +1,8 @@
+class Shopkeeper
+
+  def initialize(char_id, item_id, item_type, action_type)
+
+  end
+
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require "commander"
+
 
 # Initialize the rails application
 CharMaker::Application.initialize!
