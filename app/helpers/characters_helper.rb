acc3750f7322db262d1d36198b9017d1885199fa
socjopata <socjopata@gmail.com>
2012-04-04 18:40:05 +0200

    playing with pdf a bit

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,3 +1,27 @@
 # -*- encoding : utf-8 -*-
 module CharactersHelper
+
+  def render_character_sheet(character)
+    case character.profession.general_type
+      when "scholar" then
+        render :partial => 'characters/character_sheets/scholar' #, :locals => locals
+      when "rogue" then
+        render :partial => 'characters/character_sheets/rogue' #, :locals => locals
+      when "soldier" then
+        render :partial => 'characters/character_sheets/soldier' #, :locals => locals
+    end
+  end
+
+  def display_character_sheet_name(character)
+    case character.profession.general_type
+      when "scholar" then
+        "Uczonych"
+      when "rogue" then
+        "Łotrowska"
+      when "soldier" then
+        "Żołnierska"
+    end
+  end
 end
+
+
