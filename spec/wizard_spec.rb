6f1411db99973d0058081c0f51240b07f5334309
socjopata <socjopata@socjopatapc.(none)>
2012-10-25 19:44:55 +0200

    sample tests

--- spec/wizard_spec.rb
+++ spec/wizard_spec.rb
@@ -10,11 +10,37 @@ describe Wizard do
   context "profession_and_origin" do
 
     context "get" do
+      it "should return proper wizard instance object" do
+        @wizard = Wizard.new(@character, "profession_and_origin")
+        @wizard.character.character_background.traits.should == []
 
-      it "should build statistics for character if they are blank" do
+        professions = ProfessionSelector.new(@character).results
+        @wizard.instance_variable_get("@professions").should == professions
 
+        profession = professions.first
+        @wizard.instance_variable_get("@profession").should == profession
+
+        countries = Profession.find_by_name(profession.name).countries
+        @wizard.instance_variable_get("@countries").should == countries
+
+        @wizard.instance_variable_get("@deities").should == DeitySelector.new(@character, countries.first.id, profession.id).deities
       end
+    end
 
+    context "post" do
+      #it "should not build statistics for character if they are present" do
+      #  params = {}  #TODO
+      #  stats = @character.build_statistics
+      #  stats.save(:validate => false)
+      #  @wizard = Wizard.new(@character, "profession_and_origin")
+      #  @wizard.character.statistics.initial_dice_roll_set.should == nil
+      #end
+      #
+      #it "should build statistics for character if they are blank" do
+      #  params = {}  #TODO
+      #  @wizard = Wizard.new(@character, "profession_and_origin", params)
+      #  @wizard.character.statistics.initial_dice_roll_set.size.should == 6
+      #end
     end
 
   end
