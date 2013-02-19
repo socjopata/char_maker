deb689b6a144e3f1427494a60ae440e1aba8046b
socjopata <socjopata@socjopatapc.(none)>
2013-02-19 15:22:34 +0100

    another view... and some jquery fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//= require jquery
-//= require jquery-migrate
+//= require jquery-1.9.1
+//= require jquery-migrate-1.1.1
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui-1.10.1.custom.min
 //= require bootstrap
