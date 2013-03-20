7147df51e0b73730b5c4e6cc2845be188fcfaa22
socjopata <socjopata@socjopatapc.(none)>
2013-03-20 21:31:19 +0100

    fixing traits

--- app/models/character_background.rb
+++ app/models/character_background.rb
@@ -31,20 +31,6 @@ class CharacterBackground < ActiveRecord::Base
     traits << Trait.find_by_name(Trait::DICE_RESULT_FOR_BLESSING[num])
   end
 
-  def draw_a_social_class
-    if self.new_record?
-      dice_roll = 1.d(Trait::DICE_TYPE)
-      case dice_roll
-        when Trait::CURSE_RANGE then
-          attach_a_curse(1.d(Trait::DICE_TYPE))
-        when Trait::BLESSING_RANGE then
-          attach_a_blessing(1.d(Trait::DICE_TYPE))
-        else
-          nil
-      end
-    end
-  end
-
   def set_origin(country_id)
     self.create_origin(:country_id => country_id)
   end
