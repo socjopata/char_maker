6abd46e3660deba40382a80631d557589477ebf6
Radek Jedryszczak <socjopata@gmail.com>
2013-09-12 22:38:21 +0200

    Display shooter skills for soldiers and rouges

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -8,7 +8,7 @@ class Skill < ActiveRecord::Base
 
   scope :basic, where(:profession_type => "default")
   scope :fetch_for_type, lambda { |type| where(:profession_type => type) }
-  scope :fetch_for_level, lambda {|level| joins(:skill_requirements).where("skill_requirements.check_applies_to = 'experience' AND  skill_requirements.value = ?", level)}
+  scope :fetch_for_level, lambda { |level| joins(:skill_requirements).where("skill_requirements.check_applies_to = 'experience' AND  skill_requirements.value = ?", level) }
 
   #IDEA how about the concept of favorite weapon? Some skills give bonuses to certain weapons and armors.
   #Each skill would give an option to set up something on a screen after skill choice step but before arment step. Favorites would clear if going backwards
@@ -20,9 +20,11 @@ class Skill < ActiveRecord::Base
       when "scholar" then
         Skill.fetch_for_type("scholar")
       when "rogue" then
-        Skill.fetch_for_type("rogue") + Skill.fetch_for_type("finesse")
+        Skill.fetch_for_type("rogue") + Skill.fetch_for_type("finesse") + Skill.fetch_for_type("shooter")
       when "soldier" then
-        Skill.fetch_for_type("soldier") + Skill.fetch_for_type(character.fight_style.name=="Finezyjny" ? "finesse" : "brutal")
+        Skill.fetch_for_type("soldier") +
+            Skill.fetch_for_type(character.fight_style.name=="Finezyjny" ? "finesse" : "brutal") +
+            Skill.fetch_for_type("shooter")
     end
 
   end
@@ -44,7 +46,7 @@ class Skill < ActiveRecord::Base
   end
 
   def self.change(character, skill, action)
-    skillset =  Skill.basic + Skill.fetch_caste_skills_for(character) + Skill.fetch_profession_skills_for(character)
+    skillset = Skill.basic + Skill.fetch_caste_skills_for(character) + Skill.fetch_profession_skills_for(character)
     non_selectable_skills_before_change = Skill.filter_nonselectable(skillset.flatten, character, character.statistics.calculate_s, character.statistics.calculate_zr, character.statistics.calculate_wt, character.statistics.calculate_int, character.statistics.calculate_wi, character.statistics.calculate_o)
     action ? skill.add_skill_for(character.id) : skill.substract_skill_from(character.id)
     commander = Commander.new(character.reload, skill, non_selectable_skills_before_change)
@@ -52,8 +54,8 @@ class Skill < ActiveRecord::Base
   end
 
   def self.calculate_free_skill_amount(character, base, intelligence_bonus, used)
-   result = base + intelligence_bonus - used - (character.character_background.traits.first.try(:name)=="Amnezja" ? 2 : 0)
-   result > 0 ? result : 0
+    result = base + intelligence_bonus - used - (character.character_background.traits.first.try(:name)=="Amnezja" ? 2 : 0)
+    result > 0 ? result : 0
   end
 
 
