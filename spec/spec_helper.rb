620c520897238476ad89bd6505381ce54d5b068e
Radek Jedryszczak <socjopata@gmail.com>
2014-08-15 17:35:48 +0200

    Configure rspec to acknowledge factory girl helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,13 +13,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 Dir[Rails.root.join("spec/shared_examples/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.include FactoryGirl::Syntax::Methods
+
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
