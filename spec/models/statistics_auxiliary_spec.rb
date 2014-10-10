b9994613af6e9c3e4eab7216942de8d1654ec05b
socjopata <socjopata@gmail.com>
2014-10-10 13:17:15 +0200

    Auxiliary specs

--- spec/models/statistics_auxiliary_spec.rb
+++ spec/models/statistics_auxiliary_spec.rb
@@ -83,4 +83,47 @@ describe Statistics do
 
     it { expect(statistics.calculate_sprinting).to eq 23 }
   end
+
+  describe '#calculate_auxiliary_bonus' do
+    let(:statistics) { Statistics.new }
+    let(:group_name) { 'test skill' }
+    let!(:stats_modifiers) { [create(:stats_modifier, modifies: 'auxiliary', group_name: group_name, value: 2)] }
+
+    before do
+      statistics.stats_modifiers << stats_modifiers
+      statistics.stub_chain(:character, :character_background, :traits, :present?).and_return(false)
+      statistics.stub(:calculate_variable_auxiliary_bonus).and_return(0)
+    end
+
+    it { expect(statistics.calculate_auxiliary_bonus(group_name)).to eq 2 }
+    #TODO additional contexts?
+  end
+
+  describe '#calculate_variable_auxiliary_bonus' do
+    let(:statistics) { Statistics.new(endurance: 20) }
+    let(:group_name) { 'Punkty Życia' }
+    let!(:stats_modifiers) { [create(:stats_modifier, modifies: 'variable_auxiliary', group_name: group_name, value: 2,
+                                     evaluated_instruction: 'Statistics::BONUS_OR_PENALTY_RANGES[calculate_wt]')] }
+    before do
+      statistics.stats_modifiers << stats_modifiers
+      statistics.stub(:calculate_wt).and_return(23)
+    end
+
+    it { expect(statistics.calculate_variable_auxiliary_bonus(group_name)).to eq 4 }
+  end
+
+  describe '#magic_resistance_shield_bonus_for' do
+    let(:statistics) { Statistics.new }
+    let(:character_shield) { double }
+    let(:character) { double }
+    let(:stats_choice) { stats_modifier.stats_choice }
+    let!(:stats_modifier) { create(:stats_modifier, modifies: 'auxiliary', group_name: 'Odporność na Magię', value: 2) }
+
+    before do
+      character.stub_chain(:character_shields, :where, :first).and_return(character_shield)
+      character_shield.stub_chain(:resource, :stats_choices, :first).and_return(stats_choice)
+    end
+
+    it { expect(statistics.magic_resistance_shield_bonus_for(character)).to eq 2 }
+  end
 end
