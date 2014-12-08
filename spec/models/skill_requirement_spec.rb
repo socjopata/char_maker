9f5a9e5e2cccb97b8f52d4f59ad98a0286baa0e7
socjopata <socjopata@gmail.com>
2014-12-08 13:33:04 +0100

    Skill requirement specs

--- spec/models/skill_requirement_spec.rb
+++ spec/models/skill_requirement_spec.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 
 describe SkillRequirement do
-
   describe '#skill_fails_to_meet_requirements' do
     let(:character) { Character.new }
     let(:skill_requirement) { SkillRequirement.new(check_applies_to: context, name: name, value: value) }
@@ -171,6 +170,60 @@ describe SkillRequirement do
 
         it { expect(skill_requirement.skill_fails_to_meet_requirements(character, 20, 20, 20, 20, 20, 20)).to eq true }
       end
+    end
+  end
+
+  describe '#make_human_readable' do
+    let(:character) { Character.new }
+    let(:skill_requirement) { SkillRequirement.new(check_applies_to: context, name: name, value: value) }
+    let(:name) { 'Whatever' }
+    let(:value) { 10 }
+
+    context 'skill' do
+      let(:context) { 'skill' }
+      let(:name) { 'Wiosłowanie' }
+
+      it { expect(skill_requirement.make_human_readable).to eq "* Umiejętność Wiosłowanie" }
+    end
+
+    context 'statistics' do
+      let(:context) { 'statistics' }
+
+      context 'single requirement' do
+        let(:name) { 'INT' }
+
+        it { expect(skill_requirement.make_human_readable).to eq "* Parametr Inteligencja musi mieć wartość conajmniej 10" }
+      end
+
+      context 'multi requirement' do
+        let(:name) { 'OR: INT,WI' }
+
+        it { expect(skill_requirement.make_human_readable).to eq "* Jeden z Parametrów, INT lub WI powinien mieć wartość conajmniej 10" }
+      end
+    end
+
+    context 'experience' do
+      let(:context) { 'experience' }
+
+      it { expect(skill_requirement.make_human_readable).to eq "* Poziom doświadczenia: 10" }
+    end
+
+    context 'social_class' do
+      let(:context) { 'social_class' }
+
+      it { expect(skill_requirement.make_human_readable).to eq "* Postać musi mieć odpowiednie pochodzenie. Zerknij do podręcznika" }
+    end
+
+    context 'caster_class' do
+      let(:context) { 'caster_class' }
+
+      it { expect(skill_requirement.make_human_readable).to eq "* Postać musi być Posiadaczem Mocy" }
+    end
+
+    context 'auxiliary' do
+      let(:context) { 'auxiliary' }
+
+      it { expect(skill_requirement.make_human_readable).to eq "* Paramter Whatever musi wynosić conajmniej 10" }
     end
   end
 end
