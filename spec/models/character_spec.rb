5a99759bebf5e10bf6cc1fb00bba556a4f2f1fbd
socjopata <socjopata@gmail.com>
2015-01-16 15:19:37 +0100

    character  specs

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -31,6 +31,26 @@ describe Character do
 
     it { expect(character.skill_choices_to_be_precised).to eq [] }
   end
+
+  describe '#make_rogue_a_finesse_fighter' do
+    let(:fightstyle) { FightStyle.new }
+
+    context 'update should happen' do
+      it do
+        character.stub_chain(:profession, :present?).and_return(true)
+        character.stub_chain(:profession, :general_type).and_return('rogue')
+        character.stub(:update_attribute)
+        fightstyle.stub(:id)
+        allow(FightStyle).to receive(:find_by_name).and_return(fightstyle)
+        character.make_rogue_a_finesse_fighter
+      end
+    end
+
+    context 'update shouldnt happen' do
+      it do
+        character.stub_chain(:profession, :present?).and_return(false)
+        character.make_rogue_a_finesse_fighter
+      end
+    end
+  end
 end
-
-
