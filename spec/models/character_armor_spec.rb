d02bdda439f2dbd2142073e2d72ee2cd193211e3
socjopata <socjopata@gmail.com>
2015-01-16 12:05:44 +0100

    character armor specs

--- spec/models/character_armor_spec.rb
+++ spec/models/character_armor_spec.rb
@@ -42,10 +42,46 @@ describe CharacterArmor do
   end
 
   describe '#calculate_dexterity_cap' do
+    context 'armor is upgraded' do
+      let(:character_armor) { CharacterArmor.new(dexterity_cap: true) }
 
+      before do
+        character_armor.stub_chain(:character, :statistics, :stats_modifiers, :select, :collect, :sum).and_return(2)
+        character_armor.stub_chain(:armor, :dexterity_cap).and_return(20)
+      end
+
+      it { expect(character_armor.calculate_dexterity_cap).to eq 25 }
+    end
+
+    context 'armor is regular' do
+      let(:character_armor) { CharacterArmor.new(dexterity_cap: false) }
+
+      before do
+        character_armor.stub_chain(:character, :statistics, :stats_modifiers, :select, :collect, :sum).and_return(2)
+        character_armor.stub_chain(:armor, :dexterity_cap).and_return(20)
+      end
+
+      it { expect(character_armor.calculate_dexterity_cap).to eq 22 }
+    end
   end
 
   describe '#required_strength' do
+    let(:character_armor) { CharacterArmor.new }
 
+    context 'requirement is present' do
+      before do
+        character_armor.stub_chain(:armor, :armament_requirements, :detect, :value).and_return(5)
+      end
+
+      it { expect(character_armor.required_strength).to eq 5 }
+    end
+
+    context 'no requirement' do
+      before do
+        character_armor.stub_chain(:armor, :armament_requirements, :detect, :value).and_return(nil)
+      end
+
+      it { expect(character_armor.required_strength).to eq '-' }
+    end
   end
 end
